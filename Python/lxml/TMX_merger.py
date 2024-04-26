e0c105cee7f70bf5572c8e8a7846a5314b5a9400
flow03 <44571106+flow03@users.noreply.github.com>
2024-04-26 21:34:06 +0300

    TMX_merger: replace_prop_tu and diff counters

--- Python/lxml/TMX_merger.py
+++ Python/lxml/TMX_merger.py
@@ -60,9 +60,11 @@ class TMX_Merger():
         self.tu_dict = {}    # Default translations
         self.alt_dict = {}   # Alternative translations
 
-        self.repeat = 0
+        # self.repeat = 0
+        self.diff = 0
         self.replace = 0
-        self.alt_repeat = 0
+        # self.alt_repeat = 0
+        self.alt_diff = 0
         self.alt_replace = 0
 
     def add_tmx(self, tmx_file):
@@ -105,31 +107,34 @@ class TMX_Merger():
                 if tu_text_pl not in self.tu_dict:
                     self.tu_dict[tu_text_pl] = tu
                 else:
-                    self.repeat += 1
-                    if self.replace_tu(tu, self.tu_dict, tu_text_pl):
-                        self.replace += 1
+                    self.replace_tu(tu, tu_text_pl)
             else: # prop
                 # print(f"prop '{get_pl_text(tu)}' знайдено")
                 prop_id = get_prop_id(tu)
                 if prop_id not in self.alt_dict:
                     self.alt_dict[prop_id] = tu
                 else:
-                    self.alt_repeat += 1
-                    if self.replace_tu(tu, self.alt_dict, prop_id):
-                        self.alt_replace += 1
+                    self.replace_prop_tu(tu, prop_id)
 
-    def replace_tu(self, tu, tu_dict, key):
+    def replace_tu(self, tu, key):
         # key = get_pl_text(tu)
-        ex_tu = tu_dict[key]
+        ex_tu = self.tu_dict[key]
         if not equal_uk(tu, ex_tu):
+            self.diff += 1
             if (get_date(tu) > get_date(ex_tu)):
-                tu_dict[key] = tu
+                self.tu_dict[key] = tu
                 # print(f"{get_date(ex_tu)} замінено на {get_date(tu)}")
-                # self.replace += 1
-                return True
+                self.replace += 1
             # else:
                 # print(f"{get_date(ex_tu)} залишено, натомість {get_date(tu)}")
-        return False
+
+    def replace_prop_tu(self, tu, id):
+        ex_tu = self.alt_dict[id]
+        if not equal_uk(tu, ex_tu):
+            self.alt_diff += 1
+            if (get_date(tu) > get_date(ex_tu)):
+                self.alt_dict[id] = tu
+                self.alt_replace += 1
 
     def create_body(self):
         for key in sorted(self.tu_dict.keys()):
@@ -144,10 +149,10 @@ class TMX_Merger():
             print(f"Всього:\t {len(self.tu_dict) + len(self.alt_dict)} ({len(self.tu_dict)} + {len(self.alt_dict)})")
         else:
             print("Всього:\t", len(self.tu_dict))
-        print("Повторів:", self.repeat)
+        print("Відмінностей:", self.diff)
         print("Замін:\t", self.replace)
         if self.alt_dict:
-            print("Альтернативних повторів:", self.alt_repeat)
+            print("Альтернативних відмінностей:", self.alt_diff)
             print("Альтернативних замін:", self.alt_replace)
 
     def write_file(self, tmx_file_path):
